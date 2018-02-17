f82059110fde063d72bda93f3641b4f6511e8133
unknown <monkeyinastripedshirt@gmail.com>
2018-02-17 12:20:11 -0500

    Initial Commit

---
+++ Giveyouaname/src/Robot.cpp
@@ -0,0 +1,92 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+//Include Spark.h
+#include <Spark.h>
+#include <Joystick.h>
+
+class Robot : public frc::IterativeRobot {
+public:
+	void RobotInit() {
+		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
+		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
+		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+		left = new frc::Spark(0);
+		right = new frc::Spark(1);
+	}
+//Create pointers (*) for left and right
+	frc::Spark *left;
+	frc::Spark *right;
+	frc::Joystick *stick;
+
+	/*
+	 * This autonomous (along with the chooser code above) shows how to
+	 * select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * GetString line to get the auto name from the text box below the Gyro.
+	 *
+	 * You can add additional auto modes by adding additional comparisons to
+	 * the
+	 * if-else structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as
+	 * well.
+	 */
+	void AutonomousInit() override {
+		m_autoSelected = m_chooser.GetSelected();
+		// m_autoSelected = SmartDashboard::GetString(
+		// 		"Auto Selector", kAutoNameDefault);
+		std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void AutonomousPeriodic() {
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void TeleopInit() {
+		stick = new frc::Joystick(0);
+		double lx, ly, rx, ry;
+	}
+
+	void TeleopPeriodic() {
+		lx = stick->GetRawAxis(0);
+		ly = stick->GetRawAxis(1);
+		rx = stick->GetRawAxis(4);
+		ry = stick->GetRawAxis(5);
+
+	}
+
+	void TestPeriodic() {}
+
+private:
+	frc::LiveWindow& m_lw = *LiveWindow::GetInstance();
+	frc::SendableChooser<std::string> m_chooser;
+	const std::string kAutoNameDefault = "Default";
+	const std::string kAutoNameCustom = "My Auto";
+	std::string m_autoSelected;
+};
+
+START_ROBOT_CLASS(Robot)
